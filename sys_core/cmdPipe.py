6c8f6e1311bc44876a7836a9e3bb83472d6910a8
ErikOwsiak <erik@iotech.systems>
2024-01-07 07:09:43 +0100

    init load

--- sys_core/cmdPipe.py
+++ sys_core/cmdPipe.py
@@ -1,9 +1,12 @@
 
-import serial
+import serial as ser
+import threading as th
+# -- -- -- --
+from datatypes.serInfo import serInfo
 
 
 class cmdPipe(object):
 
    def __init__(self):
-      self.ser_from_air: serial.Serial = serial.Serial()
+      self.ser_from_air: ser.Serial = ser.Serial()
       self.ser_to_fc = None
