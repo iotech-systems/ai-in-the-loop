86120327ac539e7488a5a149b0021a7c5778ef04
ErikOwsiak <erik@iotech.systems>
2024-01-10 11:32:43 +0100

    overlay logo added

--- sys_core/vtxStream.py
+++ sys_core/vtxStream.py
@@ -34,7 +34,7 @@ class vtxStream(object):
       vtxStream.cam.start_preview(Preview.DRM)
       vtxStream.cam.start(show_preview=True)
       if os.path.exists(vtxStream.logoimg):
-         logo_overlay = cv2.imread(vtxStream.logoimg, cv2.IMREAD_COLOR)
+         logo_overlay = cv2.imread(vtxStream.logoimg, cv2.IMREAD_UNCHANGED)
          vtxStream.cam.set_overlay(logo_overlay)
       else:
          print(f"PathNotFound: {vtxStream.logoimg}")
