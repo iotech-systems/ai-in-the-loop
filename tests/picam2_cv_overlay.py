2e602c42792b893caeae292789fc8b32f5f5e73c
ErikOwsiak <erik@iotech.systems>
2024-01-10 03:56:43 +0100

    work on picam tests

---
+++ tests/picam2_cv_overlay.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import cv2, time
+from picamera2 import Picamera2, MappedArray
+
+
+picam2 = Picamera2()
+colour = (0, 255, 0)
+origin = (0, 30)
+font = cv2.FONT_HERSHEY_SIMPLEX
+scale = 1
+thickness = 2
+
+
+def apply_timestamp(request):
+   timestamp = time.strftime("%Y-%m-%d %X")
+   with MappedArray(request, "main") as m:
+      cv2.putText(m.array, timestamp, origin, font, scale, colour, thickness)
+      picam2.pre_callback = apply_timestamp
+      picam2.start(show_preview=True)
+
+
+picam2.pre_callback = apply_timestamp
+picam2.start(show_preview=True)
