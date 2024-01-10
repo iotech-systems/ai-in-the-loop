c5f90073e206a05d3f3511bdaa27d7d25981460a
ErikOwsiak <erik@iotech.systems>
2024-01-10 04:20:30 +0100

    work on picam tests

--- tests/picam2_cv_overlay.py
+++ tests/picam2_cv_overlay.py
@@ -13,11 +13,11 @@ thickness = 2
 
 
 def apply_timestamp(request):
-   timestamp = time.strftime("%Y-%m-%d %X")
+   timestamp = time.strftime("%Y/%m/%d %X")
    with MappedArray(request, "main") as m:
       cv2.putText(m.array, timestamp, origin, font, scale, colour, thickness)
       picam2.pre_callback = apply_timestamp
-      picam2.start(show_preview=True)
+      # picam2.start(show_preview=True)
 
 
 picam2.pre_callback = apply_timestamp
