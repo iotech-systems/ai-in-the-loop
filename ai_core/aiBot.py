46a6e5e10bf144ca8f72078dabf44747e2702bc2
ErikOwsiak <erik@iotech.systems>
2024-01-10 07:38:10 +0100

    work on picam tests

--- ai_core/aiBot.py
+++ ai_core/aiBot.py
@@ -50,7 +50,7 @@ class aiBot(object):
                self.vtx_stream.vtxoverlay.targ_box_color = sysColors.red
             elif b'kbd.Key.down' in bmsg:
                self.vtx_stream.vtxoverlay.ai_mode = "pilot"
-               self.vtx_stream.vtxoverlay.targ_box_thickness = 1
+               self.vtx_stream.vtxoverlay.targ_box_thickness = 2
                self.vtx_stream.vtxoverlay.targ_box_color = sysColors.sleep
             return 0
          except Exception as e:
--- sys_core/sysColors.py
+++ sys_core/sysColors.py
@@ -4,4 +4,4 @@ class sysColors(object):
    red = (255, 0, 0)
    green = (0, 255, 0)
    blue = (0, 0, 255)
-   sleep = (100, 100, 100)
+   sleep = (80, 80, 80)
