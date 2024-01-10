b3cf60beeca499129a2dca46c54f757d34fe18c5
ErikOwsiak <erik@iotech.systems>
2024-01-10 13:42:35 +0100

    overlay logo added

--- sys_core/vtxOverlay.py
+++ sys_core/vtxOverlay.py
@@ -9,7 +9,7 @@ from sys_core.sysColors import sysColors
 col_green = (0, 255, 0)
 dts_org = (20, 30)
 mode_org = (380, 30)
-baro_org = (200, 400)
+baro_org = (160, 440)
 targ_org_s = (210, 130)
 targ_org_e = (430, 350)
 font = cv2.FONT_HERSHEY_SIMPLEX
