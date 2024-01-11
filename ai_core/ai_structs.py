a87e154152e991994fa271c6c1f33ee648d33299
ErikOwsiak <erik@iotech.systems>
2024-01-11 16:40:00 +0100

    overlay logo added

--- ai_core/ai_structs.py
+++ ai_core/ai_structs.py
@@ -18,7 +18,7 @@ from collections import deque
 
 class aiModes(object):
 
-   m: [] = ["KILL/CM", "KILL/SM", "SEEK/KILL" "TRK/V0", "TRK/V1", "TRK/V2", "TRK/ONSCR"]
+   m: [] = ["KILL/CM", "KILL/SM", "SEEK/KILL", "TRK/V0", "TRK/V1", "TRK/V2", "TRK/ONSCR"]
 
    def __init__(self):
       self.modes: deque = deque()
