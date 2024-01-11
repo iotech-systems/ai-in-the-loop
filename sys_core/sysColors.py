383eee4ece1055e5e0f8561b50e956a0e80aa94c
ErikOwsiak <erik@iotech.systems>
2024-01-12 00:44:13 +0100

    overlay logo added

--- sys_core/sysColors.py
+++ sys_core/sysColors.py
@@ -2,6 +2,7 @@
 
 class sysColors(object):
    red = (255, 0, 0)
+   red_1 = (186, 6, 27)
    green = (0, 255, 0)
    blue = (0, 0, 255)
    sleep = (180, 180, 188)
@@ -11,7 +12,7 @@ class sysColors(object):
    @staticmethod
    def str_to_color(buff: str):
       if "kill" in buff.lower():
-         return sysColors.red
+         return sysColors.red_1
       if "trk" in buff.lower():
          return sysColors.d_yellow
       if "rtn" in buff.lower():
